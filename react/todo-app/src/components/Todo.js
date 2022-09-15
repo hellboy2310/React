e5cdbc7c44d6c8bb7dbf595c4cfda0c029a420a6
hellboy2310 <bhaveshpurohit2310@gmail.com>
2022-09-15 17:38:29 +0530

    todo completed

--- react/todo-app/src/components/Todo.js
+++ react/todo-app/src/components/Todo.js
@@ -2,9 +2,51 @@ import { Component } from "react";
 
 
 class Todo extends Component{
+    constructor(){
+        super();
+        this.state = {
+            tasks:[],
+            currTask:""
+        }
+    }
+    handleChange = (e) => {
+        console.log(e.target.value);
+        this.setState({
+            currTask:e.target.value
+        })
+    }
+    handleAddTask = () => {
+        this.setState({
+            tasks:[...this.state.tasks,{task:this.state.currTask,id:this.state.tasks.length + 1}],
+            currTask :""
+        })
+    }
+
+    handledelete = (id) =>{
+        let narr = this.state.tasks.filter((taskObj)=>{
+            return taskObj.id != id
+        })
+        this.setState({
+            tasks:[...narr]
+        })
+    }
+    
+    
     render(){
         return(
-            <div>This is Todo component</div> 
+            <div>
+            <input type="text" value = {this.state.currTask} onChange = {this.handleChange}/>
+            <button onClick = {this.handleAddTask}>Add task</button>
+            <ul>
+                {this.state.tasks.map((taskObj)=>(
+                    <li key = {taskObj.id}>
+                        <p>{taskObj.task}</p>
+                        <button onClick = {() =>this.handledelete(taskObj.id)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
+            </div>
+            
         )
     }
 }
